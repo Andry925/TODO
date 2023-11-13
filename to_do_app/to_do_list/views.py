95730268cd50411aa59f0bf69d7b2241b9d9b11d
Andrew925 <andry925@ukr.net>
2023-11-13 21:13:57 +0200

    Added remove task function

--- to_do_app/to_do_list/views.py
+++ to_do_app/to_do_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from . models import Tasks
 
 
@@ -17,4 +17,10 @@ def create_new_task(request):
         declared_task = request.POST["add_a_task"]
         Tasks.objects.create(task= declared_task)
     return redirect("home")
+
+def delete_task(request,pk):
+    get_task = get_object_or_404(Tasks,pk=pk)
+    get_task.delete()
+    return redirect("home")
+
 
