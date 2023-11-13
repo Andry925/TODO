48eb025b9c3aafa07211a4e649ad67099b7384fb
Andrew925 <andry925@ukr.net>
2023-11-13 22:24:55 +0200

    Added some basic form to change task (not finished yet)

--- to_do_app/to_do_list/views.py
+++ to_do_app/to_do_list/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from . models import Tasks
 
-
-
-# Create your views here.
 
 def home(request):
     all_data = Tasks.objects.all()
@@ -12,15 +9,24 @@ def home(request):
     }
     return render(request, "to_do_list/home.html", context)
 
+
 def create_new_task(request):
     if request.method == "POST":
         declared_task = request.POST["add_a_task"]
-        Tasks.objects.create(task= declared_task)
-    return redirect("home")
-
-def delete_task(request,pk):
-    get_task = get_object_or_404(Tasks,pk=pk)
-    get_task.delete()
+        Tasks.objects.create(task=declared_task)
     return redirect("home")
 
 
+def delete_task(request, pk):
+    task_to_delete = get_object_or_404(Tasks, pk=pk)
+    task_to_delete.delete()
+    return redirect("home")
+
+
+def change_task(request, pk):
+    if request.method == "GET":
+        task_to_dispay = get_object_or_404(Tasks, pk=pk)
+        context = {
+            "task_to_display": task_to_dispay
+        }
+        return render(request, "to_do_list/edit_task.html", context)
