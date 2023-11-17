217715f2af79b451fbc5e8f9def47c963b307831
Andrew925 <andry925@ukr.net>
2023-11-17 22:09:29 +0200

    Added correct url for homepage

--- to_do_app/to_do_list/urls.py
+++ to_do_app/to_do_list/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path("add_new_task/", views.create_new_task, name="add_new_task"),
     path("delete_task/<int:pk>/", views.delete_task, name="delete_task"),
     path("change_task/<int:pk>/", views.change_task, name="change_task"),
